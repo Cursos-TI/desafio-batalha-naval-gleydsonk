b49c1ffdc977514847d6ae78cf4880400a9ab581
Gleydson Fernandes <gleydson.kleber@gmail.com>
2025-09-22 20:15:07 +0000

    certo

--- batalha_naval_novato.c
+++ batalha_naval_novato.c
@@ -21,7 +21,7 @@ int main() {
 
     int linha_h = 1;
     printf("Posicionamento navio horizontal na linha %d (colunas 1 a 3)...\n", linha_h);
-    for (int j = 1; j < 3; j++){
+    for (int j = 1; j <= 3; j++){
         if (tabuleiro[linha_h][j] == 0){
             tabuleiro[linha_h][j] = 1;
             printf("Navil Horizontal -> Coordenada: (%d, %d)\n", linha_h, j);
@@ -33,14 +33,15 @@ int main() {
     int coluna_v = 2;
     printf("Posicionamento navio vertical na coluna %d (linhas 2 a 4)...\n", coluna_v);
     for (int i = 2; i <= 4; i++){
-        if (tabuleiro[i][coluna_v] == 0)
+        if (tabuleiro[i][coluna_v] == 0){
             tabuleiro[i][coluna_v] = 2;
-        printf("Navio Vertical -> Coordenada: (%d, %d)\n", i, coluna_v);
+            printf("Navio Vertical -> Coordenada: (%d, %d)\n", i, coluna_v);
+        }
     }
 
     // Exibicao do tabuleiro
     printf("\nTabuleiro (5x5):\n");
-    printf("Legenda: 0=vazio | 1=horizontal | 2=vertical | 3=sobreposicao\n\n");
+    printf("Legenda: 0=vazio | 1=horizontal | 2=vertical \n\n");
 
     for (int i = 0; i < linhas; i++){
         for (int j = 0; j < colunas; j++){
